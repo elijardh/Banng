41ef3d956f681c2446a3758810cd1af352a6a065
Dare <elijardh@gmail.com>
2020-12-20 11:18:36 +0100

    Things are looking up

--- lib/Controller/Services.dart
+++ lib/Controller/Services.dart
@@ -1,10 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'package:bang/Model/CharacterProfile.dart';
 import 'dart:convert';
+import 'dart:math';
+
 class Popular{
   List<CharacterProfile> popularHeroes = [];
   Future<void> getPopular() async {
-    List<int> ids = [1, 2, 3, 4, 5, 6];
+    List<int> ids = [69, 620, 644, 263, 306, 641,720,659,346,332,149,78,289,583,233];
     for (int i = 0; i < ids.length; i++) {
       String url = "https://superheroapi.com/api/1907388712734798/${ids[i]}";
       var response = await http.get(url);
@@ -14,4 +16,46 @@ class Popular{
       }
     }
   }
+}
+class Villains{
+  List<CharacterProfile> popularVil = [];
+  Future<void> getPopular() async {
+    List<int> ids = [60, 204, 731, 459, 216, 445, 675, 655, 273, 321, 237, 398, 405, 687, 463];
+    for (int i = 0; i < ids.length; i++) {
+      String url = "https://superheroapi.com/api/1907388712734798/${ids[i]}";
+      var response = await http.get(url);
+      if(response.statusCode == 200){
+        CharacterProfile characterProfile = CharacterProfile.fromJson(json.decode(response.body));
+        popularVil.add(characterProfile);
+      }
+    }
+  }
+}
+
+class RandomHeroes{
+  List<CharacterProfile> randomHeroes = [];
+  randomGen(int min, int max){
+
+    var ran = Random();
+
+    var x = ran.nextDouble() * (max - min) + min;
+
+    return x.floor();
+  }
+  Future<void> getRandom() async
+  {
+    List<int> randomHeroId = [];
+    for(int i = 0; i<10; i++){
+      randomHeroId.add(randomGen(1, 700));
+    }
+    for(int i = 0; i<randomHeroId.length; i++){
+      String url = "https://superheroapi.com/api/1907388712734798/${randomHeroId[i]}";
+      var response = await http.get(url);
+      if(response.statusCode == 200){
+        CharacterProfile characterProfile = CharacterProfile.fromJson(json.decode(response.body));
+        randomHeroes.add(characterProfile);
+      }
+    }
+  }
+
 }
--- lib/View/LandingPage.dart
+++ lib/View/LandingPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bang/Model/CharacterProfile.dart';
 import 'package:bang/Controller/Services.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -8,12 +10,21 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  final curpage = ValueNotifier<int>(0);
   bool isloading = true;
   List<CharacterProfile> popHeroes = new List<CharacterProfile>();
+  List<CharacterProfile> randHeroes = new List<CharacterProfile>();
+  List<CharacterProfile> popVil = new List<CharacterProfile>();
 
   getList() async {
     Popular pop = new Popular();
+    RandomHeroes randomHeroes = new RandomHeroes();
+    Villains vill = new Villains();
     await pop.getPopular();
+    await randomHeroes.getRandom();
+    await vill.getPopular();
+    popVil = vill.popularVil;
+    randHeroes = randomHeroes.randomHeroes;
     popHeroes = pop.popularHeroes;
     setState(() {
       isloading = false;
@@ -27,57 +38,196 @@ class _LandingPageState extends State<LandingPage> {
     getList();
   }
 
+  dynamic hello(int hey, CarouselPageChangedReason wtf){
+    curpage.value = hey;
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: null, child: Icon(Icons.search, color: Colors.white,),backgroundColor: Color.fromRGBO(203,65,11,1),),
+        backgroundColor: Colors.black,
           appBar: AppBar(
-            title: Container(
-              child: TextField(
-                decoration: InputDecoration(
-                  disabledBorder: UnderlineInputBorder(),
-                  hintText: "Search",
-                ),
-              ),
-            ),
+            title: Text("Bang"),
+            backgroundColor:Color.fromRGBO(203,65,11,1),
           ),
           body: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  Text("Popular Heroes"),
-                  Container(
-                      child: isloading
-                          ? Center(
-                              child: CircularProgressIndicator(),
-                            )
-                          : Container(
-                        height: 100,
-                            child: ListView.builder(itemBuilder: (context, index) {
-                              return popheroes(
-                                //popHeroes[index].image.url,
-                                popHeroes[index].name
-                              );
-                      },scrollDirection: Axis.horizontal,),
-                          ))
-                ],
-              ),
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.orange,
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Center(
+                            child: Container(
+                              margin: EdgeInsets.only(top:4),
+                              height: 11,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(203,65,11,1),
+                                borderRadius: BorderRadius.circular(20)
+                              ),
+                              
+                              child: Center(child: Text("Explore", style: TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),)),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                              left: 140,
+                              child: pageInd()),
+                          Container(
+                              child: isloading
+                                  ? Center(
+                                child: CircularProgressIndicator(),
+                              )
+                                  : Container(
+                                padding: EdgeInsets.all(20),
+                                decoration: BoxDecoration(
+                                ),
+                                width: MediaQuery.of(context).size.width,
+                                height: MediaQuery.of(context).size.height*0.45,
+                                child: CarouselSlider.builder(itemBuilder: (context, index) {
+                                  return randomChar(
+                                      randHeroes[index]
+                                  );
+                                },itemCount: popHeroes.length,
+                                options: CarouselOptions(
+                                  height: MediaQuery.of(context).size.height*0.45,
+                                  autoPlay: true,
+                                  enlargeCenterPage: true,
+                                  pageSnapping: true,
+                                  enableInfiniteScroll: true,
+                                  scrollDirection: Axis.horizontal,
+                                  onPageChanged: hello,
+                                ),),
+                              )),
+
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Container(child: Text("Popular Heroes", style: TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold),), padding: EdgeInsets.only(left:10), decoration: BoxDecoration(color: Color.fromRGBO(203,65,11,1),), width: 400,),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      height: 150,
+                        child: ListView.builder(itemBuilder: (BuildContext context, index){
+                          return popheroes(popHeroes[index]);
+                        },
+                          itemCount: popHeroes.length,
+                          padding: EdgeInsets.all(10),
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                        )
+                    ),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Container(child: Text("Popular Villains", style: TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold),), padding: EdgeInsets.only(left:10), decoration: BoxDecoration(color: Color.fromRGBO(203,65,11,1),), width: 400,),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        height: 150,
+                        child: ListView.builder(itemBuilder: (BuildContext context, index){
+                          return popheroes(popVil[index]);
+                        },
+                          itemCount: popVil.length,
+                          padding: EdgeInsets.all(10),
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                        )
+                    ),
+
+                  ],
+                ),
+              ],
             ),
-          )),
+          )
+        )
     );
   }
 
-  Widget popheroes(String charactername){
+  Widget randomChar(
+      CharacterProfile characterProfile){
     return Container(
-      child: Stack(
-        children: <Widget>[
-          //Image.network(imageurl),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(charactername),
-          )
-        ],
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height*0.25,
+      child:Container(child: Image.network(characterProfile.image.url,fit:BoxFit.cover,), decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20)
+      ),)
+    );
+  }
+
+  Widget popheroes(CharacterProfile characterProfile){
+    return Container(
+      height: 150,
+      width: 90,
+      padding: EdgeInsets.all(2),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(203,65,11,1),
+            borderRadius: BorderRadius.circular(10)
+        ),
+
+        padding: EdgeInsets.all(5),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              child: Image.network(characterProfile.image.url, fit: BoxFit.cover,),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 15,
+                width: 90,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.8),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(characterProfile.name, style: TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget pageInd(){
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(padding: EdgeInsets.all(10), child: CirclePageIndicator(
+        itemCount: randHeroes.length,
+        currentPageNotifier: curpage,
+      ),),
     );
   }
 }
